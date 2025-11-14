272d678ebdeffebdad797b68ebb2bfbb1220810d
HIMAWARIKM <mori@himawari-lpc.com>
2025-11-15 01:01:29 +0900

    Add files via upload

---
+++ 交通費アプリ/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  // ここにキャッシュなどの処理を書けるが、
+  // とりあえず何もしない
+});
